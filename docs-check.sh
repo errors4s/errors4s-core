71d1b4d7b5a6dddce946e6637833c9b3f41dd24d
David Strawn <isomarcte@gmail.com>
2021-05-26 10:14:55 -0600

    Add Check To Ensure Generated Documentation Is Up To Date

---
+++ docs-check.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if git update-index --refresh && git diff-index --quiet @ --
+then
+    sbt ';docs/clean;docs/mdoc'
+    if git update-index --refresh && git diff-index --quiet @ --
+    then
+        echo 'Generated documents under VCS control are up to date.'
+        exit 0
+    else
+        echo 'Generated documents under VCS control are not up to date.' 1>&2
+        exit 2
+    fi
+else
+    echo 'There are local changes. The docs-check.sh may only be executed in a clean git environment.' 1>&2
+    git status
+    exit 1
+fi
