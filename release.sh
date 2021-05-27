f08c8b03f26127714d892d361e5ba229d8140b74
David Strawn <isomarcte@gmail.com>
2021-05-27 13:12:50 -0600

    Use Scala 2.13 For Scalafmt/Scalafix In Release

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 source sbt-release-script/src/bash/functions.sh
 
-export SBT_TASKS=';scalafixAll --check;+scalafmtSbtCheck;+scalafmtCheckAll;+test;+test:doc;'
+export SBT_TASKS='++2.13.6;scalafixAll --check;scalafmtSbtCheck;scalafmtCheckAll;++3.0.0;+test;+test:doc;'
 readonly SBT_TASKS
 
 sbt_release
